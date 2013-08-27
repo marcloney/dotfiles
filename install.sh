f0613c5c8711b05f26ed4e29d0654b7ec12bdaaa
Marc Loney <marc@marcloney.com>
2013-08-27 05:02:42 +0000

    Added vim to install script

--- install.sh
+++ install.sh
@@ -3,13 +3,18 @@
 # Create symlinks to .files
 rm -f ~/.ssh/config
 rm -f ~/.bashrc
-rm -f ~/.gitconfig
 rm -f ~/.bash_profile
+rm -f ~/.bash_logout
+rm -f ~/.gitconfig 
+rm -rf ~/.vim
+rm -f ~/.vimrc
 
 ln -s ~/bin/dotfiles/ssh/config ~/.ssh/config
 ln -s ~/bin/dotfiles/bashrc ~/.bashrc
 ln -s ~/bin/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/bin/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/bin/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/bin/dotfiles/vim ~/.vim
 
 # Change ownership of local (Tip from @izs)
 # sudo chown -vF marcloney:marcloney /usr/local
